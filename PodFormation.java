9df53ae37e6315a73c3beecb6b8ca69bd3842b31
ayushroychowdhury <70759984+ayushroychowdhury@users.noreply.github.com>
2024-11-13 17:00:45 -0600

    Create PodFormation.java

---
+++ PodFormation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PodFormation {
+    public PodFormation(StudentGraph graph) {
+        // Constructor
+    }
+
+    public void formPods(int podSize) {
+        // Method signature only
+    }
+}
