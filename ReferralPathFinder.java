4348f41b520ef6ef4ae041b3f171834a140c405c
ayushroychowdhury <70759984+ayushroychowdhury@users.noreply.github.com>
2024-11-13 17:00:29 -0600

    Create ReferralPathFinder.java

---
+++ ReferralPathFinder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ReferralPathFinder {
+    public ReferralPathFinder(StudentGraph graph) {
+        // Constructor
+    }
+
+    public List<UniversityStudent> findReferralPath(UniversityStudent start, String targetCompany) {
+        // Method signature only
+        return new ArrayList<>();
+    }
+}
