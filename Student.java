4997c86c6c6bbdc5e27306d937770395b219d4b2
ayushroychowdhury <70759984+ayushroychowdhury@users.noreply.github.com>
2024-11-13 16:57:01 -0600

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public abstract class Student {
+    protected String name;
+    protected int age;
+    protected String gender;
+    protected int year;
+    protected String major;
+    protected double gpa;
+    protected List<String> roommatePreferences;
+    protected List<String> previousInternships;
+
+    public abstract int calculateConnectionStrength(Student other);
+}
