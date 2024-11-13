02b17f01ff5fc0fc2247fc9276871492a38947ad
ayushroychowdhury <70759984+ayushroychowdhury@users.noreply.github.com>
2024-11-13 16:56:19 -0600

    Create DataParser.java

---
+++ DataParser.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class DataParser {
+    public static List<UniversityStudent> parseStudents(String filename) throws IOException {
+        return new ArrayList<>();
+    }
+}
