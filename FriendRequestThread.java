986430190e6464298dbd627e9eeb5dbb14eee72a
ayushroychowdhury <70759984+ayushroychowdhury@users.noreply.github.com>
2024-11-13 17:01:11 -0600

    Create FriendRequestThread.java

---
+++ FriendRequestThread.java
@@ -0,0 +1,10 @@
+public class FriendRequestThread implements Runnable {
+    public FriendRequestThread(UniversityStudent sender, UniversityStudent receiver) {
+        // Constructor
+    }
+
+    @Override
+    public void run() {
+        // Method signature only
+    }
+}
