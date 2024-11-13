a8653115bbdfb6c80f82317cebc7799284ee5ed3
ayushroychowdhury <70759984+ayushroychowdhury@users.noreply.github.com>
2024-11-13 17:01:38 -0600

    Create ChatThread.java

---
+++ ChatThread.java
@@ -0,0 +1,10 @@
+public class ChatThread implements Runnable {
+    public ChatThread(UniversityStudent sender, UniversityStudent receiver, String message) {
+        // Constructor
+    }
+
+    @Override
+    public void run() {
+        // Method signature only
+    }
+}
